38c6431a5da437fece8cdd9e6b8fd79e40855ab5
prinzzy <murfidkamil01@gmail.com>
2021-09-28 09:14:50 +0700

    commit on master

---
+++ application/views/v_about.php
@@ -0,0 +1,23 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Murfid</li>
+        <li>Nama Belakang : Dzakwan Kamil</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Pandan Valley Bogor</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Tanggerang</li>
+    </ul>
+    <h4>Olah Raga Favorit</h4>
+    <ul type="square">
+        <li>Bulutangkis</li>
+        <li>Lari</li>
+    </ul>
+</section>
